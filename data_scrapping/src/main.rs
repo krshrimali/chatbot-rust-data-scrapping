442f4b00f28ab42382c7f160f0f39294da697e6c
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2024-02-20 23:22:18 +0530

    Add progress bar and append document text to a file

--- data_scrapping/src/main.rs
+++ data_scrapping/src/main.rs
@@ -7,13 +7,18 @@ use soup::NodeExt;
 use soup::Soup;
 // use log::{debug, info, error, warn};
 
+use indicatif::ProgressBar;
+
 pub fn extract_all_hyperlinks(
     raw_data: Result<String, std::io::Error>,
     all_links: &mut Vec<String>,
+    all_text: &mut Vec<String>,
 ) -> Result<(), std::io::Error> {
     match raw_data {
         Ok(content) => {
             let soup = Soup::new(&content);
+            let text = soup.text();
+            all_text.push(text);
             let all_tags_a_href = soup.tag("a").find_all();
 
             all_tags_a_href.enumerate().for_each(|(_, tag)| {
@@ -60,7 +65,9 @@ fn main() {
     env_logger::init();
 
     let mut all_links: Vec<String> = Vec::new();
-    let if_succeeded = extract_all_hyperlinks(raw_data, &mut all_links);
+    let mut all_text: Vec<String> = Vec::new();
+
+    let if_succeeded = extract_all_hyperlinks(raw_data, &mut all_links, &mut all_text);
     let mut nested_links: Vec<String> = Vec::new();
     if let Err(e) = if_succeeded {
         log::error!("Error: {:?}", e);
@@ -68,19 +75,52 @@ fn main() {
     }
 
     let mut visited_set: HashSet<String> = HashSet::new();
+    let mut total_visited_alr_and_skipped: u32 = 0;
+    let mut total_visited_and_successful: u32 = 0;
+    let mut total_failed: u32 = 0;
+
+    // progress bar in rust
+    let pb = ProgressBar::new(all_links.len() as u64);
     for link in all_links.iter() {
         if visited_set.contains(link) {
             log::info!("Skipping link because it's alr visited: {:?}", link);
+            total_visited_alr_and_skipped += 1;
+            pb.inc(1);
             continue;
         }
         let modified_link = Path::new(&main_html_link).parent().unwrap().join(link);
         let content = fetch_raw_html(modified_link.display().to_string());
-        let success = extract_all_hyperlinks(content, &mut nested_links);
+        let success = extract_all_hyperlinks(content, &mut nested_links, &mut all_text);
         visited_set.insert(link.clone());
         if success.is_ok() {
             log::info!("Success for link: {:?}", link);
+            total_visited_and_successful += 1;
             log::debug!("Nested links: {:?}", nested_links);
+        } else {
+            log::info!("Failed for link: {:?}", link);
+            total_failed += 1;
         }
+        pb.inc(1);
     }
     log::info!("All links: {:?}", all_links);
+
+    println!("Statistics: ");
+    println!(
+        "Total visited and successful: {:?}",
+        total_visited_and_successful
+    );
+    println!(
+        "Total visited and skipped: {:?}",
+        total_visited_alr_and_skipped
+    );
+    println!("Total failed: {:?}", total_failed);
+    println!("All text fetched: {:?}", all_text.len());
+
+    // write all_text to a single file
+    let text_pb = ProgressBar::new(all_text.len() as u64);
+    let file_name = "output.txt";
+    for text in all_text.iter() {
+        std::fs::write(file_name, text).expect("Failed to write the file");
+        text_pb.inc(1);
+    }
 }
